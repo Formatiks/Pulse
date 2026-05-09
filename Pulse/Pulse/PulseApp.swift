06f7f3cc93b0c01da028ebc50ad1947193d3e772
SuperFico2100 <superfico2100@gmail.com>
2026-05-09 17:22:02 +0200

    Initial commit

---
+++ Pulse/Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Edoardo Tolone on 09/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
